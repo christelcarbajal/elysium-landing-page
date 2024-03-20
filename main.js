ec09232d97d88fdd06e2ab97e1e439646b09d553
Christel Carbajal <chrdeveloper02@gmail.com>
2024-03-20 23:55:36 +0100

    added landing page

--- main.js
+++ main.js
@@ -13,3 +13,58 @@ navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  // header container
+  ScrollReveal().reveal(".header__container p", {
+    ...scrollRevealOption,
+  });
+  
+  ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  // about container
+  ScrollReveal().reveal(".about__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+  
+  ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+  ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+  });
+  
+  ScrollReveal().reveal(".about__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+  });
+  
+  ScrollReveal().reveal(".about__btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
+  
+  // room container
+  ScrollReveal().reveal(".room__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  // service container
+  ScrollReveal().reveal(".service__list li", {
+    ...scrollRevealOption,
+    interval: 500,
+    origin: "right",
+  });
